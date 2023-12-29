93f934ab43e60d912853b459d5134de931f99ac0
ryanbeevers <beeversryan@gmail.com>
2023-12-29 11:36:11 -0500

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAVf-yKI4bx7F8Go9XHqC8oI_nfbksXMB0",
+    authDomain: "finance-app-ui-c8539.firebaseapp.com",
+    projectId: "finance-app-ui-c8539",
+    storageBucket: "finance-app-ui-c8539.appspot.com",
+    messagingSenderId: "878790212670",
+    appId: "1:878790212670:web:e38482da5c7d28a7e2e56f",
+    measurementId: "G-RSR4LHZCY6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAVf-yKI4bx7F8Go9XHqC8oI_nfbksXMB0",
+    authDomain: "finance-app-ui-c8539.firebaseapp.com",
+    projectId: "finance-app-ui-c8539",
+    storageBucket: "finance-app-ui-c8539.appspot.com",
+    messagingSenderId: "878790212670",
+    appId: "1:878790212670:web:e38482da5c7d28a7e2e56f",
+    measurementId: "G-RSR4LHZCY6"
+  }
 };
 
 /*
