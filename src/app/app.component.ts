fd5ff9d46f981eb17771377af72e9bbc46ac85fe
ryanbeevers <beeversryan@gmail.com>
2023-12-29 14:53:27 -0500

    Updating to test UI connection to db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'finances-app-ui';
+  editObj: any;
+  personalInfo: any;
+  @ViewChild('btnShow')
+  btnShow!: ElementRef;
+  @ViewChild('btnClose')
+  btnClose!: ElementRef;
+  dataSource: any;
+  name: any;
+
+  constructor(private store: AngularFirestore) {
+
+  }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this.store.collection('userInfo').snapshotChanges().subscribe((response) => {
+      this.dataSource = response.map(item =>
+        Object.assign({ id: item.payload.doc.id }, item.payload.doc.data())
+      );
+    })
+  }
+
+  add() {
+    if (this.editObj) {
+      this.store.collection('userInfo').doc(this.editObj.id).update({ name: this.name, personalInfo: this.personalInfo });
+    } else {
+      this.store.collection('userInfo').add({ name: this.name, personalInfo: this.personalInfo });
+    }
+    this.closeDialog();
+  }
+
+  edit(id: string) {
+    this.store.collection('userInfo').doc(id).get().subscribe((response) => {
+      this.editObj = Object.assign({ id: response.id }, response.data());
+      this.name = this.editObj.name;
+      this.personalInfo = this.editObj.personalInfo;
+      this.openDialog();
+    })
+  }
+
+  clearEdit() {
+    this.editObj = null;
+    this.name = "";
+    this.personalInfo = "";
+  }
+
+  delete(id: string) {
+    this.store.collection('list').doc(id).delete();
+  }
+
+  openDialog() {
+    this.btnShow.nativeElement.click();
+  }
+
+  closeDialog() {
+    this.btnClose.nativeElement.click();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from  '@angular/fire/compat';
+import { AngularFirestoreModule } from  '@angular/fire/compat/firestore';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAVf-yKI4bx7F8Go9XHqC8oI_nfbksXMB0",
+      authDomain: "finance-app-ui-c8539.firebaseapp.com",
+      projectId: "finance-app-ui-c8539",
+      storageBucket: "finance-app-ui-c8539.appspot.com",
+      messagingSenderId: "878790212670",
+      appId: "1:878790212670:web:e38482da5c7d28a7e2e56f",
+      measurementId: "G-RSR4LHZCY6"
+    }),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
