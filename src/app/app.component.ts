366674616804631cf43bf225370ebcf175e9978d
ryanbeevers <beeversryan@gmail.com>
2024-01-07 15:45:39 -0500

    removed starting code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,65 +9,13 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 export class AppComponent implements OnInit {
   
   title = 'finances-app-ui';
-  editObj: any;
-  personalInfo: any;
-  @ViewChild('btnShow')
-  btnShow!: ElementRef;
-  @ViewChild('btnClose')
-  btnClose!: ElementRef;
-  dataSource: any;
-  name: any;
 
-  constructor(private store: AngularFirestore) {
+
+  constructor() {
 
   }
 
   ngOnInit(): void {
-    this.getAll();
-  }
-
-  getAll() {
-    this.store.collection('userInfo').snapshotChanges().subscribe((response) => {
-      this.dataSource = response.map(item =>
-        Object.assign({ id: item.payload.doc.id }, item.payload.doc.data())
-      );
-    })
-  }
-
-  add() {
-    if (this.editObj) {
-      this.store.collection('userInfo').doc(this.editObj.id).update({ name: this.name, personalInfo: this.personalInfo });
-    } else {
-      this.store.collection('userInfo').add({ name: this.name, personalInfo: this.personalInfo });
-    }
-    this.closeDialog();
-  }
-
-  edit(id: string) {
-    this.store.collection('userInfo').doc(id).get().subscribe((response) => {
-      this.editObj = Object.assign({ id: response.id }, response.data());
-      this.name = this.editObj.name;
-      this.personalInfo = this.editObj.personalInfo;
-      this.openDialog();
-    })
-  }
-
-  clearEdit() {
-    this.editObj = null;
-    this.name = "";
-    this.personalInfo = "";
-  }
-
-  delete(id: string) {
-    this.store.collection('list').doc(id).delete();
-  }
-
-  openDialog() {
-    this.btnShow.nativeElement.click();
-  }
-
-  closeDialog() {
-    this.btnClose.nativeElement.click();
   }
 
 }
